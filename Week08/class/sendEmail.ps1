e4e403d97dd836be740fa6ffca551f2ee81ce3f0
Trevor Amell <31705834+t-amell@users.noreply.github.com>
2021-03-19 15:31:22 -0400

    Completed Week08 Homework Assignment

---
+++ Week08/class/sendEmail.ps1
@@ -0,0 +1,16 @@
+﻿# Storyline: Send an email.
+
+# Variable can have an underscore or any alphanumeric value.
+# Body of the email
+$msg = "Hello There."
+
+Write-Host -BackgroundColor Red -ForegroundColor White $msg
+
+# Email From Address
+$email = "trevor.amell@mymail.champlain.edu"
+
+# To Address
+$toEmail = "deployer@csi-web"
+
+# Sending the email
+Send-MailMessage -From $email -To $toEmail -Subject "A Greeting" -Body $msg -SmtpServer 192.168.6.71
---
+++ Week08/homework/searchLog.ps1
@@ -0,0 +1,18 @@
+﻿# Storyline: Review the Security Event Log
+
+# Directory to save files
+
+$myDir = "C:\Users\Trevo\Desktop\"
+
+#List all the available windows Event logs
+Get-EventLog -List
+
+# Create a prompt to allow user to select the log to view
+$readLog = Read-Host -Prompt "Please select a log to review from the list above"
+
+# Create prompt to get keyword/phrase from user to search on
+$searchFor = Read-Host -Prompt "Please specify a search term to find within the $readLog list"
+
+#Print the results for the log
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$searchFor*" } | export-csv -NoTypeInformation `
+-Path "$myDir\securityLog.csv"
